6d06c902d8e0634bcd9f6c137b3cdd3ed1ff2990
Guille80 <guillermobaez@gmail.com>
2025-11-26 00:06:29 +0100

    implemented cli to json

--- Minifrwk/src/minifrwk/cli.py
+++ Minifrwk/src/minifrwk/cli.py
@@ -24,14 +24,10 @@ def suma(sumatoria: str = typer.Option(..., "--sumatoria", help="Ruta al archivo
 @app.command()
 def suma2(config: str = typer.Option(..., "--config", help="Ruta al archivo de configuración YAML")):
     """Suma dos números contenidos en la lista "numeros" en el archivo de configuración ."""
+    import json
     with open(config, 'r') as f:
         cfg = yaml.safe_load(f)
-        typer.echo(cfg)
-        numeros = cfg["numeros"]
-        typer.echo(f"Números a sumar: {numeros}")
-        total = sum(numeros)
-        typer.echo(f"La suma de los números es: {total}")
-
+        print(json.dumps(cfg, indent=4))
 
 
 if __name__ == "__main__":
