98750feacfc32193c47dc6ee276cd133167f68a5
Guille80 <guillermobaez@gmail.com>
2025-11-26 00:14:14 +0100

    implemented validations in cli

--- Minifrwk/src/minifrwk/cli.py
+++ Minifrwk/src/minifrwk/cli.py
@@ -29,6 +29,44 @@ def suma2(config: str = typer.Option(..., "--config", help="Ruta al archivo de c
         cfg = yaml.safe_load(f)
         print(json.dumps(cfg, indent=4))
 
+@app.command()
+def validate1(config: str = typer.Option(..., "--config", help="Ruta al archivo de configuración YAML")):
+    """Valida tipos en el archivo de configuración YAML."""
+    from minifrwk.config_loader.loader import load_yaml, print_validation_result
 
+    data = load_yaml(config)
+    print("Datos cargados desde YAML:", data)
+
+    print("Validando tipos YAML:")
+
+    print_validation_result('seed', data['seed'], 'int')
+    print_validation_result('rounds', data['rounds'], 'int')
+    print_validation_result('round_name', data['round_name'], 'int')
+    print_validation_result('clients', data['clients'], 'dict')
+    print_validation_result('clients.total', data['clients']['total'], 'int')
+    print_validation_result('clients.per_round', data['clients']['per_round'], 'int')
+    print_validation_result('clients.lr', data['clients']['lr'], 'float')
+    print_validation_result('algorithm', data['algorithm'], 'dict')
+
+@app.command()
+def validate(config: str = typer.Option(..., "--config", help="Ruta al archivo de configuración YAML")):
+    """Valida tipos en el archivo de configuración YAML."""
+    from minifrwk.config_loader.loader import load_yaml, print_validation_result
+
+    data = load_yaml(config)
+    with open(config, 'r') as f:
+        cfg = yaml.safe_load(f)
+        faltantes = []
+        obligatorios = ["seed", "rounds", "clients", "algorithm", "logging", "data"]
+        for campo in obligatorios:
+            if campo not in cfg:
+                faltantes.append(campo)
+        if faltantes:
+            typer.echo(f"Faltan campos obligatorios: {', '.join(faltantes)}")
+            raise typer.Exit(code=1)
+        else:
+            typer.echo("Todos los campos obligatorios están presentes.")
+
+        
 if __name__ == "__main__":
     app()
